dcef4bde601aa8da6960692b80e5927035174faa
carson-loser <carson.loser@gmail.com>
2022-08-31 17:08:25 -0500

    started the README file

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# password-generator
+# Password Generator
+
+#### By Carson Loser
+
+#### This application provides the user with a password with specifications to their liking.
+
+# Prerequisites and Dependencies
+
+* When user clicks on "Generate Password", presented with input dependent prompts for password criteria.
+
+* When presented a prompt for password length, user must choose a value between 8 and 128. Anything under or over will not work.
+
+* Prompts following character length should be seperate choice prompts of uppercase letters, lowercase letters, numeric values, and/or special characters.
+
+* The inputs chosen by the user should be validated and at least one prompt should be selected when all prompts are answered. 
+
+* Once all prompts are answered, a password should be generated that fits all selected criteria.
+
+* The password is dsiplayed on the page or written as an alert. 
+
+
+# Live Server URL
+
+- https://carson-loser.github.io/first-portfolio/
+
+# Assets
+
+The following image demonstrates the web application's appearance and functionality:
+
+![Shows a heading with company name and functional nav bar. Images load clearly and correctly](./assets/Portfolio-SC.png)
--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -4,6 +4,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   // if statement allows code to return out of writePassword function if it is empty or doesnt meet beginning criteria
   // will not log "undefined" when entering an unaccepted value into a prompt
   if (!password) {
@@ -18,24 +19,29 @@ function writePassword() {
 function generatePassword() {
   var passwordLength = prompt("Choose length, 8-128");
   console.log(passwordLength);
+
   // leaves passwordOptions empty so that it can be filled out as the user makes their selections accordingly
   var passwordOptions = "";
-// creates an if statement where if the inputted values is less than 8 OR greater than 128, user will be prompted to pick a new number to fit criteria
+
+  // creates an if statement where if the inputted values is less than 8 OR greater than 128, user will be prompted to pick a new number to fit criteria
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Please pick a number between 8 and 128")
-    return 
+    return
   }
-// creates variables and confirm prompts for each type of requested type of character
+
+  // creates variables and confirm prompts for each type of requested type of character
   var lowercase = confirm("Would you like lowercase characters in your password?");
   var uppercase = confirm("Would you like uppercase characters in your password?");
   var numeric = confirm("Would you like numeric values in your password?");
   var specialCharacters = confirm("Would you like special characters in your password?");
-// logs each variable that is tied to a prompt in order
+
+  // logs each variable that is tied to a prompt in order
   console.log(lowercase);
   console.log(uppercase);
   console.log(numeric);
   console.log(specialCharacters);
-// if statements (if selected) will add the passwordOptions (which has no value currently) with one selected character from the string
+
+  // if statements (if selected) will add the passwordOptions (which has no value currently) with one selected character from the string
   if (lowercase == true) {
     passwordOptions = passwordOptions + "abcdefghijklmnopqrstuvwxyz"
   }
@@ -63,7 +69,7 @@ function generatePassword() {
     console.log(password);
   } return password
 
-  
+
 
 
 
